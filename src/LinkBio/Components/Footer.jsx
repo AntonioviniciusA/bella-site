c6a508b300e22ddf1817a688af9125528a13906a
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-02 21:57:17 -0300

    `Updated copyright and license information in Footer component and replaced Logo image.`

--- src/LinkBio/Components/Footer.jsx
+++ src/LinkBio/Components/Footer.jsx
@@ -2,8 +2,7 @@ export function Footer() {
   return (
     <div className="text-center pb-8 px-4">
       <p className="text-slate-400 text-sm">
-        © 2024 Sistemas Inteligentes. Transformando negócios através da
-        tecnologia.
+        © 2025 Todos direitos reservados. Bella Soluções
       </p>
     </div>
   );
